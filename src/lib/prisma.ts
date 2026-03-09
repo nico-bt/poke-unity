0ec6ba293bd4da918432a7a978c1ca03bf84d7a0
Nico-Battaglia <nico-battaglia@hotmail.com>
2026-03-09 10:46:10 -0300

    Add Prisma integration with PostgreSQL. Add schema

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+// Avoid multiple Prisma Client instances
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../db/generated/prisma/client";
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
