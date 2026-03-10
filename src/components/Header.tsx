c0ad7714d81a4a1e4526680cc016121dc55a8cd1
Nico-Battaglia <nico-battaglia@hotmail.com>
2026-03-10 00:44:34 -0300

    Add Ash image to header and update navigation label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ function Header({ session }: { session: SessionType }) {
           priority
         />
         <span className="font-semibold text-[11px] self-center uppercase tracking-widest">
-          Home
+          Home | Pokemons
         </span>
       </Link>
 
@@ -42,11 +42,11 @@ function Header({ session }: { session: SessionType }) {
       ) : (
         <div className="flex gap-4 flex-1">
           <div className="mx-auto flex gap-4 items-center">
-            <span>Admin</span>
+            <Image src={"/ash.jpg"} height={50} width={50} alt="Ash" />
             {pathname !== "/admin" && (
               <Link
                 href={"/admin"}
-                className="bg-green-800 hover:bg-green-900 px-3 py-1 rounded-lg"
+                className="bg-green-800 hover:bg-green-900 px-4 py-2 rounded"
               >
                 Add Card
               </Link>
